34a1020f91fa0e4705c4ae7936f324dd7561fe88
ThisIsNotNotAValidUsername <nmarcum@unomaha.edu>
2024-11-24 01:35:05 -0600

    Update television.py

--- television.py
+++ television.py
@@ -12,50 +12,50 @@ class Television:
     self.__channel = Television.MIN_CHANNEL
     self.__volume = Television.MIN_VOLUME
 
-  def power(self):
+  def power(self) -> None:
     # Method to turn on/off the TV
     self.__status = not self.__status
 
-  def mute(self):
+  def mute(self) -> None:
     # Method to mute the TV
     if self.__status:
       self.__mute = not self.__mute
 
-  def channel_up(self):
+  def channel_up(self) -> None:
     # Method to increase channel
     if self.__status:
-      if self.__channel == Television.MAX_CHANNEL:
-        self.__channel = Television.MIN_CHANNEL
+      if self.__channel == self.MAX_CHANNEL:
+        self.__channel = self.MIN_CHANNEL
       else:
         self.__channel += 1
 
-  def channel_down(self):
+  def channel_down(self) -> None:
     # Method to decrease channel
     if self.__status:
-      if self.__channel == Television.MIN_CHANNEL:
-        self.__channel = Television.MAX_CHANNEL
+      if self.__channel == self.MIN_CHANNEL:
+        self.__channel = self.MAX_CHANNEL
       else:
         self.__channel -= 1
 
-  def volume_up(self):
+  def volume_up(self) -> None:
     # Method to increase TV volume
     if self.__status:
       if self.__mute:
         self.mute()
-      if self.__volume != Television.MAX_VOLUME:
+      if self.__volume != self.MAX_VOLUME:
         self.__volume += 1
 
-  def volume_down(self):
+  def volume_down(self) -> None:
     # Method to reduce TV volume
     if self.__status:
       if self.__mute:
         self.mute()
-      if self.__volume != Television.MIN_VOLUME:
+      if self.__volume != self.MIN_VOLUME:
         self.__volume -= 1
 
   def __str__(self) -> str:
   # Method to return TV status
     vol = self.__volume
     if self.__mute:
-      vol = Television.MIN_VOLUME
+      vol = 0
     return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {vol}'
